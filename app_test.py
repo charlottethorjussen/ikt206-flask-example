3cb32fa2da74eebf4676c25940ace8c4f7ee9f36
charlottethorjussen <charlotte.thorjussen@hotmail.com>
2023-05-24 14:53:18 +0200

    testing with new test

--- app_test.py
+++ app_test.py
@@ -5,9 +5,9 @@ import json
 
 class FlaskAppTests(unittest.TestCase):
 
-    def test_title(site):
-        site.app.get(f'{site.base_url}')
-        assert "Flask Example" in site.title
+      def client(self):
+        tested_app.app.config['TESTING'] = True
+        self.app = tested_app.app.test_client()
 
 
 if __name__ == '__main__':
