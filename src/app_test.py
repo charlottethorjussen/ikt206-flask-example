b912bcddde3dbcc5a18ad1ae263e96f339721cf5
Charlotte Celine Thorjussen <charlotte.thorjussen@hotmail.com>
2023-05-25 13:01:59 +0200

    tester

--- src/app_test.py
+++ src/app_test.py
@@ -1,14 +1,14 @@
-import unittest
-import app as tested_app
-import json
+# import unittest
+# import app as tested_app
+# import json
 
 
-class FlaskAppTests(unittest.TestCase):
+# class FlaskAppTests(unittest.TestCase):
 
-      def client(self):
-        tested_app.app.config['TESTING'] = True
-        self.app = tested_app.app.test_client()
+#       def client(self):
+#         tested_app.app.config['TESTING'] = True
+#         self.app = tested_app.app.test_client()
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
